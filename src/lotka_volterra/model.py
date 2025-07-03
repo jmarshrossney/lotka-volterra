9e0acdfbbf3e86868305fe13f2f9582f5c0d0654
jmarshrossney <17361029+jmarshrossney@users.noreply.github.com>
2025-07-03 12:05:45 +0100

    add docstrings; slightly tidy up run script

--- src/lotka_volterra/model.py
+++ src/lotka_volterra/model.py
@@ -1,14 +1,22 @@
 from dataclasses import dataclass, field, fields
-from os import PathLike
 from random import randrange
-from typing import Any, Callable, Self
+from typing import Any, Callable
 
 import numpy as np
 from scipy.integrate import solve_ivp
-from scipy.integrate._ivp.ivp import OdeResult
+from scipy.integrate._ivp.ivp import OdeResult  # for type annotation only
 
 @dataclass
 class Parameters:
+    """
+    Parameters for the Lotka-Volterra model.
+
+    Parameters:
+      alpha: The base growth rate for prey.
+      beta: Coefficient of predation.
+      gamma: The base death rate for predators.
+      delta: The influence of prey on the predator growth rate.
+    """
     alpha: float
     beta: float
     gamma: float
@@ -26,6 +34,13 @@ class Parameters:
 
 @dataclass
 class InitialConditions:
+    """
+    Initial conditions for the Lotka-Volterra model.
+
+    Parameters:
+      x0: Initial population of prey.
+      y0: Initial population of predators.
+    """
     x0: float = field(default_factory=lambda: randrange(10, 20))
     y0: float = field(default_factory=lambda: randrange(1, 8))
     
@@ -39,15 +54,36 @@ class InitialConditions:
 type VectorField = Callable[[float, tuple[float, float]], tuple[float, float]]
 
 def vector_field(α: float, β: float, γ: float, δ: float) -> VectorField:
+    """
+    Constructs a vector field on the RHS of the Lotka-Volterra equations.
+
+        d(x, y)/dt = f(t, (x, y))
+
+    where
+
+        dx/dt = αx - βxy ; dy/dt = -γy + δxy
+
+    Note that there is no time (t) dependence.
+    """
     def f(t: float, x_y: tuple[float, float]) -> tuple[float, float]:
         x, y = x_y
         dxdt = α * x - β * x * y
         dydt = -γ * y + δ * x * y
         return (dxdt, dydt)
+
     return f
 
 
 def integrate(f: VectorField, x0_y0: tuple[float, float], t1: float, **kwargs) -> OdeResult:
+    """
+    A thin wrapper for scipy.integrate.solve_ivp that integrates a vector field.
+
+    Parameters:
+      f: A vector field with signature f(t, (x, y))
+      x0_y0: A tuple containing the initial (t=0) states for x & y
+      t1: The time to integrate up to
+      kwargs: Additional arguments to pass to `solve_ivp`.
+    """
     return solve_ivp(
         fun=f,
         t_span=(0, t1),
--- src/lotka_volterra/scripts/run.py
+++ src/lotka_volterra/scripts/run.py
@@ -1,43 +1,59 @@
+from datetime import datetime
+import pathlib
+
 from jsonargparse import ArgumentParser
 import matplotlib.pyplot as plt
 import numpy as np
 
 from lotka_volterra.model import Parameters, InitialConditions, vector_field, integrate
 
+def filename() -> str:
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H%M%S")
+    return f"{timestamp}.png"
+
+
 parser = ArgumentParser()
 parser.add_argument("--parameters", type=Parameters)
 parser.add_argument("--init_conds", type=InitialConditions)
-parser.add_argument("--t1", type=float, default=100)
+parser.add_argument("--t1", type=float, default=100, help="Integration window size, [0, t1]")
+parser.add_argument("-o", "--output", type=pathlib.Path, default=pathlib.Path(filename()), help="file path to save plot.")
 
 parser.add_argument("-c", "--config", action="config")
 
 def main(config: dict | None = None) -> None:
+    """
+    Integrates the Lotka-Volterra system and saves a plot.
+    """
     if config is None:
+        # Parse args from the command line
         config = parser.parse_args()
 
+    print("Config\n------\n" + parser.dump(config))
+
+    # Instantiate `Parameters` and `InitialConditions` classes
     config = parser.instantiate_classes(config)
 
-    print(config)
-
+    # Unpack config object
     α, β, γ, δ = config.parameters.to_tuple()
     x0_y0 = config.init_conds.to_tuple()
     t1 = config.t1
 
     f = vector_field(α, β, γ, δ)
 
-    solution = integrate(f, x0_y0, t1, dense_output=True, max_step=1e-2)
-
-    print(solution)
+    result = integrate(f, x0_y0, t1, dense_output=True, max_step=1e-2)
+    
+    # `result` contains a bunch of info. We just want the solution.
+    solution = result.sol
 
     t = np.linspace(0, t1, 1000)
-    x, y = solution.sol(t)
+    x, y = solution(t)
 
     fig, (ax1, ax2) = plt.subplots(1, 2)
     ax1.plot(t, x, label="x")
     ax1.plot(t, y, label="y")
-    ax1.legend()
     ax2.plot(x, y)
-    fig.savefig("result.png")
+    ax1.legend()
+    fig.savefig(config.output)
 
 
 if __name__ == "__main__":
